564d294f530f7d8efadd8722536de44dcbef9d5f
arslanbht <mohammad.arslan@beaconhousetechnology.com>
2025-12-02 18:27:05 +0500

    set the default page

--- Program.cs
+++ Program.cs
@@ -75,7 +75,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Admin}/{action=Dashboard}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages(); // Required for Identity UI
 
